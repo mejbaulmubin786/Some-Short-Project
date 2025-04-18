00828f2541080f4e55cb9e334e134d4f62b831e2
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-04-19 00:10:42 +0600

    update

--- 15 Project Laravel/01.SimpleBlogApplication/resources/views/posts/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
-    <a href="{{ route('posts.index') }}" class="btn btn-secondary">Back</a>
-@endsection
