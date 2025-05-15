50cb9a4a48260f8c0562adf6c267b5bc0cb6e970
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-05-15 06:08:46 +0600

    update

--- project7/stopwatch2/script.js
+++ project7/stopwatch2/script.js
@@ -5,11 +5,11 @@ btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains('startButton')) {
-      btn.style.color = 'red';
+      // btn.style.color = 'red';
     } else if (styles.contains('resetButton')) {
-      btn.style.color = 'blue';
+      // btn.style.color = 'blue';
     } else {
-      btn.style.color = 'green';
+      // btn.style.color = 'green';
     }
 
   })
